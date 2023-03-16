13ed9a1f53d028732321ff93c5afd1aae8e4cffc
Tetiana2023 <drofaj18@gmail.com>
2023-03-16 19:15:15 +0200

    first part

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
+import { Component } from "react";
+import { Searchbar } from "./Searchbar/Searchbar";
+
+import css from './App.module.css';
+
+export class App extends Component {
+
+  formSubmit = data => {
+    console.log(data)
+
+  }
+
+render(){
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.app}>
+     <Searchbar onSubmit={this.formSubmit}/>
+      
     </div>
   );
+}
+  
 };
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+import {FcSearch} from 'react-icons/fc';
+import css from './Searchbar.module.css'
+
+export class Searchbar extends Component {
+  state = {
+    inputValue: '',
+  };
+
+  hendleInputChange = event =>{
+    this.setState({inputValue: event.currentTarget.value.toLowerCase()})
+  }
+
+  hendleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.inputValue);
+    this.setState({inputValue: ''});
+
+  }
+  render() {
+    return (
+      <header className={css.searchbar}>
+        <form className={css.searchForm} onSubmit={this.hendleSubmit}>
+          <button type="submit" className={css.searchFormbutton} >
+           <FcSearch style={{marginRight:8, width:"20", height:"20"}}/>
+          </button>
+
+          <input
+            className={css.searchForminput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.hendleInputChange}
+            value={this.state.inputValue}
+          />
+        </form>
+      </header>
+    );
+  }
+}
